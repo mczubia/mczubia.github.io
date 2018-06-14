c84ef51ae536a012b4f5e51273486cbca45c18bd
mczubia <38670478+mczubia@users.noreply.github.com>
2018-06-14 10:23:47 -0700

    6/14
    thank you
    severity

--- assignments/lesson-8/scripts/severityslider.js
+++ assignments/lesson-8/scripts/severityslider.js
@@ -5,3 +5,4 @@ output.innerHTML = slider.value; // Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
     output.innerHTML = this.value;
+}
