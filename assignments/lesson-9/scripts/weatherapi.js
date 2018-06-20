fb8c0eab6a7a70f5c91183a7bb303831de0627f7
mczubia <38670478+mczubia@users.noreply.github.com>
2018-06-19 21:14:33 -0700

    Franklin weather summary

--- assignments/lesson-9/scripts/weatherapi.js
+++ assignments/lesson-9/scripts/weatherapi.js
@@ -1,4 +1,4 @@
-var weatherRequest= new XMLHttpRequest
+/*var weatherRequest= new XMLHttpRequest
 weatherRequest.open('GET', '//api.openweathermap.org/data/2.5/weather?id=4156210&appid=4f59fd300aa5f89148c3195a3f920e62&units=imperial',true);
 
 weatherRequest.send();
@@ -7,4 +7,42 @@ weatherRequest.onload= function() {
    var weatherData= JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
     document.getElementById("current-temp").innerHTML= weatherData.main.temp;
+}*/
+
+var weatherObject= new XMLHttpRequest
+weatherObject.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=4156210&appid=4f59fd300aa5f89148c3195a3f920e62&units=imperial',true);
+
+
+//4623560
+weatherObject.send();
+
+weatherObject.onload= function() {
+    
+    
+    
+   var weatherInfo= JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+    
+    document.getElementById('place').innerHTML= weatherInfo.name;
+    
+    document.getElementById('weatherDesc').innerHTML= weatherInfo.weather[0].main;
+    
+    document.getElementById('highTemp').innerHTML= weatherInfo.main.temp_max;
+    
+    document.getElementById('lowTemp').innerHTML= weatherInfo.main.temp_min;
+    
+    document.getElementById('currentTemp').innerHTML= weatherInfo.main.temp;
+    
+    document.getElementById('currentWind').innerHTML= weatherInfo.wind.speed;
+    
+    
+    
+    var iconcode= weatherInfo.weather[0].icon;
+    
+    var icon_path= "http://openweathermap.org/img/w/"+ iconcode +".png";
+    document.getElementById('weather_icon').src= icon_path;
 }
+//end of the function
+
+
+//remove http just before loading to GitHub or page won't work on
