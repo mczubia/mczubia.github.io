8da287a3ae77ded60d274736a74741d4f39e5a83
mczubia <38670478+mczubia@users.noreply.github.com>
2018-06-26 12:02:56 -0700

    nav

--- assignments/weather-site/scripts/active.js
+++ assignments/weather-site/scripts/active.js
@@ -5,7 +5,7 @@
         pageHREF = urlArray[urlArray.length - 1];
 
         if (pageHREF !== "") {
-            menu = document.querySelectorAll('ul#mainmenu li a');
+            menu = document.querySelectorAll('ul#primaryNav li a');
             for (i = 0; i < menu.length; i++) {
                 currentURL = (menu[i].getAttribute('href'));
                 menu[i].parentNode.className = '';
